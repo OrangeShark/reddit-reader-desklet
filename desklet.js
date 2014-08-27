947c6c65b12e25ccc48256b84bc0bef4ae444f79
Erik Edrosa <erik.edrosa@gmail.com>
2014-08-27 00:21:12 -0400

    Add GPL license and reorganize code
    Apply gnome's style guide for JS.
    Add subreddit label to the desklet.
    Change the style a bit of the desklet.

--- desklet.js
+++ desklet.js
@@ -1,11 +1,29 @@
+/* Cinnamon desklet for reading reddit.com
+
+Copyright (C) 2014 Erik Edrosa
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
+const Soup = imports.gi.Soup;
+const St = imports.gi.St;
+const Util = imports.misc.util;
+
 const Desklet = imports.ui.desklet;
 const Settings = imports.ui.settings;
-const Lang = imports.lang;
-const St = imports.gi.St;
-const Gtk = imports.gi.Gtk;
-const Soup = imports.gi.Soup;
-const Mainloop = imports.mainloop;
-const Util = imports.misc.util;
 
 const MIN_TO_MS = 60 * 1000;
 
@@ -57,11 +75,14 @@ Subreddit.prototype = {
     _processResponse: function(error, result) {
         if(error !== null) {
             this.posts = [];
+            global.logError("reddit-reader: Recieved a %d from %s".format(
+                        error, this.url));
         } else {
             let resultPosts = result.data.children;
             this.posts =
                 resultPosts.map(function(c) { return new Post(c.data); });
         }
+
         if(this.onLoad) {
             this.onLoad();
         }
@@ -114,7 +135,7 @@ RedditDesklet.prototype = {
         try {
             this.settings = new Settings.DeskletSettings(
                     this, this.metadata.uuid, this.instance_id);
-            // property for subreddits
+
             this.settings.bindProperty(
                     Settings.BindingDirection.IN,
                     "subreddit",
@@ -150,53 +171,67 @@ RedditDesklet.prototype = {
     },
     
     setupUI: function() {
-        this._redditBox = new St.BoxLayout({vertical: true,
-                                            width: this.width,
-                                            height: this.height,
-                                            style_class: "reddit-reader"});
+        // main container of the desklet
+        this._redditBox = new St.BoxLayout( {vertical: true,
+                                             width: this.width,
+                                             height: this.height,
+                                             style_class: "reddit-reader"} );
         
         let titlebar = new St.BoxLayout({vertical: false,
                                          style_class: "reddit-title-bar"});
+        
         let icon = new St.Icon({
             icon_name: "web-browser-symbolic",
             style_class: "reddit-header-icon"});
+        
         let headerButton = new St.Button();
-        let name = new St.Label({ text: "reddit" });
         headerButton.add_actor(icon);
         headerButton.connect("clicked", function(button, event) {
             Util.spawnCommandLine("xdg-open http://www.reddit.com");
         });
+
+        let name = new St.Label({ text: "reddit",
+                                  style_class: "reddit-title" });
+
+        this._subname = new St.Label( {text: "loading...",
+                                       style_class: "reddit-subtitle" });
+
         titlebar.add(headerButton);
         titlebar.add(name);
+        titlebar.add(this._subname);
         this._redditBox.add(titlebar);
 
         this._view = new St.ScrollView();
-        this._redditBox.add(this._view, { expand: true });
         this._view.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC);
-
+        this._redditBox.add(this._view, { expand: true });
+        
+        this.setContent(this._redditBox);
     },
 
     draw: function() {
         if(this._postBox) {
             this._postBox.destroy();
         }
-        this._postBox = new St.BoxLayout({ vertical: true,
-                                           style_class: "reddit-posts-box"});
+
+        this._subname.set_text(this.subreddit);
+        this._postBox = new St.BoxLayout( { vertical: true,
+                                            style_class: "reddit-posts-box"} );
         this._view.add_actor(this._postBox);
 
         let posts = this.model.getPosts();
         for(let i = 0; i < posts.length; i++) {
+            // St buttons are used as containers for the clicked event
             let postButton = new St.Button({style_class: "reddit-post",
                                             x_align: St.Align.START});
+            // bind to post to have access to the url
             postButton.connect("clicked", Lang.bind(posts[i], function(b, e) {
                 Util.spawnCommandLine("xdg-open %s".format(this.url));
             }));
-            let postLabel = new St.Label({text: posts[i].title});
+            let postLabel = new St.Label( {text: posts[i].title} );
             postButton.add_actor(postLabel);
             this._postBox.add(postButton);
         }
 
-        this.setContent(this._redditBox);
     },
 
     _onSubredditChange: function() {
@@ -205,12 +240,14 @@ RedditDesklet.prototype = {
     },
 
     _onSizeChange: function() {
+        // destory UI and reset it
         this._redditBox.destroy();
         this.setupUI();
         this.draw();
     },
 
     _updateLoop: function() {
+        // check if a timeout already exists
         if(this.update_id) {
             Mainloop.source_remove(this.update_id);
             this.update_id = 0;
